aff8b13109fb30b2e2584780ef627775d5e39d1d
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-03-30 11:31:01 -0300

    :sparkles: creating a Header, and styling and add images

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.scss'
+
+export const Header = () => (
+  <header className={styles.container}>
+    <img src="pet-friendly.png" alt="Logo Help Dog" />
+    <h1>HelpDog</h1>
+  </header>
+)
